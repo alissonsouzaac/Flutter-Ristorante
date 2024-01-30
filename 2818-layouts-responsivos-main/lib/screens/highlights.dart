10950c2005ff45dd6990353aa6f8dfa1758c7b79
Alisson <alisson.costa.external@lojayoucom.com.br>
2024-01-30 13:56:59 -0300

    update

---
+++ 2818-layouts-responsivos-main/lib/screens/highlights.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:panucci_ristorante/cardapio.dart';
+import 'package:panucci_ristorante/components/highlight_item.dart';
+
+class Highlights extends StatelessWidget {
+  const Highlights({Key? key}) : super(key: key);
+  final List items = destaques;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: CustomScrollView(
+        slivers: <Widget>[
+          SliverToBoxAdapter(child: Text("Destaques")),
+          SliverList(
+            delegate: SliverChildBuilderDelegate((context, index) {
+              return HighlightItem(
+                imageURI: items[index]["image"],
+                itemTitle: items[index]["name"],
+                itemPrice: items[index]["price"],
+                itemDescription: items[index]["description"],
+              );
+            }, childCount: items.length),
+          )
+        ],
+      ),
+      // child: Column(
+      //   children: [
+      //     Text('Destaques'),
+      //     SizedBox(
+      //       height: MediaQuery.of(context).size.height,
+      //       child: ListView.builder(
+      //         itemBuilder: (context, index) {
+      //           return HighlightItem(
+      //               imageURI: items[index]["image"],
+      //               itemTitle: items[index]["name"],
+      //               itemPrice: items[index]["price"],
+      //               itemDescription: items[index]["description"]);
+      //         },
+      //         itemCount: items.length,
+      //       ),
+      //     ),
+      //   ],
+      // ),
+    );
+  }
+}
